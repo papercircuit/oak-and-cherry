9ae203a9df18ec0e1d90c8429d3614c7d8bc5673
Kenny Johnson <105308787+papercircuit@users.noreply.github.com>
2023-02-22 12:31:26 -0500

    Leaves set to random hue

--- src/components/LeavesSketch.js
+++ src/components/LeavesSketch.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import p5 from 'p5';
 
 const LeavesSketch = () => {
+    const [canvas, setCanvas] = useState(null);
+
     useEffect(() => {
         const sketch = (p) => {
             let leaves = [];
@@ -15,53 +17,41 @@ const LeavesSketch = () => {
                 p.colorMode(p.HSB, 360, 100, 100, 1);
                 // we use p.noStroke() to disable the drawing of borders around shapes
                 p.noStroke();
-                for (let i = 0; i < 50; i++) {
+                for (let i = 0; i < 80; i++) {
                     leaves.push(new Leaf());
                 }
             };
-
-            p.draw = () => {
-                // p.backround will clear the canvas with a color (white would be HSB(0, 0, 100, 1)
-                p.background(0, 0, 100, 1);
-                // we draw a rectangle that is centered vertically and horizontally
-                p.fill(40, 100, 60, .5);
-                p.rect(p.width / 2, p.height / 2, 100, 100);
-                for (let i = 0; i < leaves.length; i++) {
-                    leaves[i].update();
-                    leaves[i].display();
-                }
-            };
-
             class Leaf {
                 constructor() {
                     this.x = p.random(p.width);
                     this.y = p.random(-p.height, -100);
                     this.size = p.random(10, 60);
-                    this.speed = p.random(0, 3);
+                    this.speed = p.random(0.1, 3);
                     // angle is the angular position of the leaf. we use it to make the leaves move in a random direction
                     this.angle = p.random(p.TWO_PI);
                     //omega is the angular velocity. we use it to make the leaves rotate by a random amount
                     this.omega = p.random(-0.05, 0.05);
+                    this.fill = p.color(p.random(10, 50), 80, 80, 0.8);
                 }
-
                 update() {
                     // here we use the p.sin() function to make the leaves move in a random direction
                     this.x += p.sin(this.angle) * this.speed;
-                    //   here we use the p.cos() function to make the leaves rotate by a random amount
+                    // here we add omega to the angle to make the leaves rotate
                     this.angle += this.omega;
+                    // if the leaf is out of the canvas we move it back to the top
                     if (this.y > p.height + this.size) {
                         this.y = p.random(-p.height, -100);
                         this.x = p.random(p.width);
                     }
+                    // we add the speed to the y position to make the leaves move down
                     this.y += this.speed;
                 }
-
                 display() {
                     p.push();
                     p.translate(this.x, this.y);
                     p.rotate(this.angle + p.PI / 2);
                     p.scale(this.size / 100);
-                    p.fill(30, 80, 80, 0.8);
+                    p.fill(this.fill);
                     p.beginShape();
                     p.vertex(0, -60);
                     p.bezierVertex(30, -60, 30, -30, 0, 0);
@@ -70,10 +60,21 @@ const LeavesSketch = () => {
                     p.pop();
                 }
             }
+            p.draw = () => {
+                // p.backround will clear the canvas with a color (white would be HSB(0, 0, 100, 1)
+                p.background(0, 0, 100, 1);
+                // we use a for loop to iterate through the leaves array and call the update and display methods
+                for (let i = 0; i < leaves.length; i++) {
+                    leaves[i].update();
+                    leaves[i].display();
+                }
+            };
         };
 
-        new p5(sketch);
+        setCanvas(new p5(sketch));
     }, []);
+
+
 
     return <div id="leaves-sketch"></div>;
 };
